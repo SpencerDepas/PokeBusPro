eae54c2858189d4a4f8bde32b5f590c70f595160
SD <spencerdepas@gmail.com>
2015-03-10 12:25:05 -0400

    Got the prefrence screen to work. All information is currently defualt and not active.

--- app/src/main/java/clearfaun/com/pokebuspro/PrefsFragment.java
+++ app/src/main/java/clearfaun/com/pokebuspro/PrefsFragment.java
@@ -1,7 +1,9 @@
 package clearfaun.com.pokebuspro;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.util.Log;
 
 /**
  * Created by spencer on 3/9/2015.
@@ -14,5 +16,17 @@ public class PrefsFragment extends PreferenceFragment {
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        try {
+            getView().setBackgroundColor(Color.GRAY);
+        }catch (Exception e){
+            Log.i("PrefsFragment", "Eception e " + e);
+        }
+
     }
 }
