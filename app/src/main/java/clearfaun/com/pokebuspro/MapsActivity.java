cd3fdcd7179e2ed28a7d22e6876ed0b7bcaf86e6
SD <spencerdepas@gmail.com>
2015-03-26 13:23:25 -0400

    Added material design buttons. Color is off.

--- app/src/main/java/clearfaun/com/pokebuspro/MapsActivity.java
+++ app/src/main/java/clearfaun/com/pokebuspro/MapsActivity.java
@@ -477,7 +477,7 @@ public class MapsActivity extends FragmentActivity implements
             CameraPosition cameraPosition = new CameraPosition.Builder()
                     .target(latLng)    // Sets the center of the map to Mountain View
                     .zoom(17)                   // Sets the zoom
-                    .bearing(90)                // Sets the orientation of the camera to east
+                    .bearing(40)                // Sets the orientation of the camera to east
                     .tilt(30)                   // Sets the tilt of the camera to 30 degrees
                     .build();                   // Creates a CameraPosition from the builder
             mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
