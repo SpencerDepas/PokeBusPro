1807673c7a3f9aca4931a59cbef3b42734687199
SD <spencerdepas@gmail.com>
2015-04-05 17:59:34 -0400

    Added all licenses. Looks good.

--- app/src/main/java/clearfaun/com/pokebuspro/LicenseActivity.java
+++ app/src/main/java/clearfaun/com/pokebuspro/LicenseActivity.java
@@ -15,15 +15,14 @@ import java.util.Vector;
 /**
  * Created by spencer on 4/5/2015.
  */
-public class LicenseActivity extends Activity{
+public class LicenseActivity extends Activity {
 
-    String labels="caption";
-    String text="";
+    String labels = "caption";
+    String text = "";
     String[] s;
     private Vector<String> wordss;
-    int j=0;
+    int j = 0;
     private StringTokenizer tokenizer;
-
 
 
     @Override
@@ -35,37 +34,43 @@ public class LicenseActivity extends Activity{
         view.setBackgroundColor(Color.WHITE);
 
         wordss = new Vector<String>();
-        TextView helloTxt = (TextView)findViewById(R.id.textView);
-        helloTxt.setText(readTxt());
 
+        TextView[] textViewArray = new TextView[3];
+        textViewArray[0] = (TextView) findViewById(R.id.textViewOne);
+        textViewArray[1] = (TextView) findViewById(R.id.textViewTwo);
+        textViewArray[2] = (TextView) findViewById(R.id.textViewThree);
+        readTxt(textViewArray);
 
     }
 
 
-    private String readTxt(){
+    private void readTxt(TextView[] textViewArray) {
+        String names[] = new String[3];
+        InputStream inputStream[] = new InputStream[3];
+        inputStream[0] = getResources().openRawResource(R.raw.license_one);
+        inputStream[1] = getResources().openRawResource(R.raw.licensetwo);
+        inputStream[2] = getResources().openRawResource(R.raw.license_three);
+        //     InputStream inputStream = getResources().openRawResource(R.raw.internals);
+        for (int z = 0; z < inputStream.length; z ++){
+                System.out.println(inputStream);
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
 
-        InputStream inputStream = getResources().openRawResource(R.raw.license_one);
-            //     InputStream inputStream = getResources().openRawResource(R.raw.internals);
-        System.out.println(inputStream);
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-
-        int i;
-        try {
-            i = inputStream.read();
-            while (i != -1)
-            {
-                byteArrayOutputStream.write(i);
-                i = inputStream.read();
+            int i;
+            try {
+                i = inputStream[z].read();
+                while (i != -1) {
+                    byteArrayOutputStream.write(i);
+                    i = inputStream[z].read();
+                }
+                inputStream[z].close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
             }
-            inputStream.close();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            textViewArray[z].setText(byteArrayOutputStream.toString());
+
         }
 
-        return byteArrayOutputStream.toString();
     }
-
-
 
 }
