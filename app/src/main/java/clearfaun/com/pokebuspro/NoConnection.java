78d3455a62b57611a423cd90d5cc21f29224f073
Spencer Zeke Depas <spencerdepas@gmail.com>
2016-04-13 17:00:11 -0400

    Updated noConnection activity

--- app/src/main/java/clearfaun/com/pokebuspro/NoConnection.java
+++ app/src/main/java/clearfaun/com/pokebuspro/NoConnection.java
@@ -6,6 +6,8 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
@@ -14,15 +16,18 @@ import android.view.View;
 /**
  * Created by spencer on 4/2/2015.
  */
-public class NoConnection extends Activity {
+public class NoConnection extends AppCompatActivity {
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.no_connection);
+        setContentView(R.layout.no_connection_coord);
 
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
 
         Log.i("NoConnection", "onCreate");
 
