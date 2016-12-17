46f7223d5ef3196a587e0ef2ca5858f40ce10cf8
Spencer Zeke Depas <spencerdepas@gmail.com>
2016-12-17 21:14:05 +0000

    more work on call and parse refactor

--- app/src/main/java/Manager/BusStopDataManager.java
+++ app/src/main/java/Manager/BusStopDataManager.java
@@ -18,6 +18,7 @@ import client.CallAndParse;
 import model.BusStopDistances;
 import model.UkBusStopsLocation;
 import ui.activity.interfaces.NoBusesInAreaInterface;
+import utils.DistanceFormula;
 
 /**
  * Created by SpencerDepas on 12/17/16.
@@ -36,7 +37,7 @@ public class BusStopDataManager {
     private void BusStopDataManager() {
     }
 
-    public static synchronized BusStopDataManager getInstance() {
+    public static BusStopDataManager getInstance() {
         if (sBusStopDataManager == null) {
             sBusStopDataManager = new BusStopDataManager();
         }
@@ -51,7 +52,8 @@ public class BusStopDataManager {
             @Override
             public void onApiSuccess(UkBusStopsLocation ukBusStopsLocation) {
                 sBusStopsSize = ukBusStopsLocation.getStops().size();
-                if (ukBusStopsLocation.getStops().size() > 0) {
+                if (sBusStopsSize > 0) {
+
                     makeBusDistanceThreads(ukBusStopsLocation);
                 } else {
                     mNoBusINterFace.noBusesFound();
@@ -70,8 +72,11 @@ public class BusStopDataManager {
         for (int i = 0; i < ukBusStopsLocation.getStops().size(); i++) {
             LatLng busStopLatLng = new LatLng(ukBusStopsLocation.getStops().get(i).getLatitude(),
                     ukBusStopsLocation.getStops().get(i).getLongitude());
-            GetBusDistanceTask getBusDistancesLongOperation = new GetBusDistanceTask(busStopLatLng);
-            getBusDistancesLongOperation.execute(ukBusStopsLocation.getStops().get(i).getAtcocode());
+            GetBusDistanceTask getBusDistanceTask = new GetBusDistanceTask(busStopLatLng);
+            getBusDistanceTask.execute(ukBusStopsLocation.getStops().get(i).getAtcocode());
+
+
+            findClosestBusStopToCurrentLocation(busStopLatLng, ukBusStopsLocation.getStops().get(i).getAtcocode());
         }
     }
 
@@ -101,31 +106,18 @@ public class BusStopDataManager {
                 });
     }
 
-
+    //belongs in marker manager 
     private void findClosestBusStopToCurrentLocation(LatLng busStopLatLng, String busCode) {
-
         double currentDistance;
-        currentDistance = distFrom(mUserLatLng.latitude, mUserLatLng.longitude,
+        currentDistance = DistanceFormula.distFrom(mUserLatLng.latitude, mUserLatLng.longitude,
                 busStopLatLng.latitude, busStopLatLng.longitude);
 
         if (currentDistance < mClosestDistance) {
-            Log.i("MyCallAndParse", "currentDistance < mClosestDistance " + busCode);
             sClosestMarker = busCode;
             mClosestDistance = currentDistance;
         }
 
     }
 
-    private double distFrom(double lat1, double lng1, double lat2, double lng2) {
-        double earthRadius = 6371000; //meters
-        double dLat = Math.toRadians(lat2 - lat1);
-        double dLng = Math.toRadians(lng2 - lng1);
-        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-                Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
-                        Math.sin(dLng / 2) * Math.sin(dLng / 2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        float dist = (float) (earthRadius * c);
 
-        return dist;
-    }
 }
